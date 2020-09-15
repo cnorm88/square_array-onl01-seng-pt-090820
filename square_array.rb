b18ba612bffea3aa3a06ed8014b1c1b73b4022e7
Crystal Norman <cnorm88@gmail.com>
2020-09-15 21:33:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 
 def square_array(array)
   array = array ** 2
-     array.each do |number|
-     puts "#{number ** 2}"
+  new_array []
+     new_array.each do |number|
+     puts "#{number}"
       end
     
  end
